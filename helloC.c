a23a5f04ca04a364ed5f2a8e14246c10899a4ca5
Wang2367 <tranvietquang2306@gmail.com>
2024-03-07 17:29:42 +0700

    special -1

---
+++ helloC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+   int var1 = 5;
+
+   // 5 is displayed
+   // Then, var1 is increased to 6.
+   printf("%d\n", var1++);
+   printf("%d\n", var1++);
+
+   // var2 is increased to 6 
+   // Then, it is displayed.
+   
+
+   return 0;
+}   
